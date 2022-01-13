3bc8f50215ac69c8e8eaa1e96242359a08d0bc61
J Nisbet <github_dev@kissake.net>
2022-01-13 17:07:15 -0500

    First version.  Basic ratcheting works, some serious bugs, incl. default ratchet file not functional, output does not contain time info.

---
+++ clockratchet.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+# Needed for running subprocesses
+import os
+import sys
+import argparse
+
+# File containing the ratchet state.
+ratchetFileDefault="~/.clockratchet"
+
+def arguments():
+
+    parser = argparse.ArgumentParser(
+        description="Return monotonically increasing time related information for use in identifiers")
+    parser.add_argument(
+        '--ratchet',
+        action='store_true',
+        help='Increment the ratchet value, and produce no output.  This is intended for use on boot')
+    parser.add_argument(
+        '--file',
+        dest='ratchetFile',
+        default=ratchetFileDefault,
+        help='Indicate the file to store the ratchet data in.  Default value is: %s' % (ratchetFileDefault))
+    return parser.parse_args()
+
+
+def getRelTime():
+    # Get years, weeks, days, hours, minutes, seconds since boot.
+    None
+
+def getNTPSyncTime():
+    # Get current time if NTPSynchronized, or return false otherwise.
+    None
+
+def getRatchet(ratchetFileName=None):
+    # Get the current value of the ratchet/counter, default to zero (0)
+    try:
+        
+        if ratchetFileName is None:
+            ratchetFile = open(ratchetFileDefault,'r')
+        else:
+            ratchetFile = open(ratchetFileName,'r')
+
+        # retrieve and return current ratchet value
+        currentRatchet = int(ratchetFile.read())
+        return currentRatchet
+    
+    except FileNotFoundError:
+        print("The ratchet file may not have been created yet.  Please check to ensure %s has been created using the --ratchet argument to this command" % (ratchetFileName))
+        raise
+
+    except ValueError:
+        print("The ratchet file may have been corrupted.  You can overwrite the contents of %s using the --ratchet argument to this command" % (ratchetFileName))
+        raise
+        
+            
+
+    
+def ratchet(ratchetFileName=None):
+    # Increment the locally stored counter (typically boot counter)
+    oldRatchetValue = getRatchet(ratchetFileName)
+    newRatchetValue = oldRatchetValue+1
+    ratchetFile = open(ratchetFileName,'w')
+    ratchetFile.write(str(newRatchetValue))
+
+
+if __name__ == "__main__":
+    args = arguments()
+    print(args)
+    if args.ratchet:
+        ratchet(args.ratchetFile)
+    else:
+        ratchetValue=getRatchet(args.ratchetFile)
+        relTimeValue=getRelTime()
+        timeSyncValue=getNTPSyncTime()
+        if timeSyncValue != False:
+            print("%s+%s--%s" % (ratchetValue, relTimeValue, timeSyncValue))
+        else:
+            print("%s+%s" % (ratchetValue, relTimeValue))
+        

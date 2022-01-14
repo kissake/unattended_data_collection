3e4e4af242bd18f706ec3c5c6c5c7c3251875a2e
J Nisbet <github_dev@kissake.net>
2022-01-13 22:18:42 -0500

    Added clockratchet functionality to RecordMp3.sh script.

--- RecordMp3.sh
+++ RecordMp3.sh
@@ -17,11 +17,13 @@ MICROPHONE="${4}"
 # Original format: S16_LE
 FORMAT="U8"
 
+RATCHETFILE="/home/pi/.clockratchet"
+
 ### User feedback:
 # Turn off the LED to indicate the device is busy by setting it not to trigger on anything. (reverts to static setting of brightness, default 0)
 echo none | sudo tee /sys/class/leds/led0/trigger > /dev/null
 
-/usr/bin/ffmpeg -t "${SECS}" -f alsa -ac 1 -ar 44100 -i "${MICROPHONE}" -acodec libmp3lame -f mp3 pipe:1  2> /dev/null | /home/pi/encrypt.sh "${KEYID}" "${DESTDIR}"/`date -Iseconds`.mp3.gpg 2> /dev/null
+/usr/bin/ffmpeg -t "${SECS}" -f alsa -ac 1 -ar 44100 -i "${MICROPHONE}" -acodec libmp3lame -f mp3 pipe:1  2> /dev/null | /home/pi/encrypt.sh "${KEYID}" "${DESTDIR}"/`/home/pi/clockratchet.py --file "${RATCHETFILE}"`.mp3.gpg 2> /dev/null
 sync
 
 # Restore previous (activity and power) function of the LED.  This means the LED should be on when the mic is not recording.
